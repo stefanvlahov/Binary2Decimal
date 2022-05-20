2a1cf0f1509f6d81aa0bc1090895a86cfd4ded5f
svlahov <stefan.vlahov5@gmail.com>
2022-05-19 20:58:47 -0400

    cosmetic updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 let binaryInput = document.getElementById("binary");
 let entryForm = document.getElementById("form_submission");
 
-entryForm.addEventListener("submit", function (f) {
-  f.preventDefault();
+entryForm.addEventListener("submit", false);
+
+binaryInput.addEventListener("", handler, false);
+
+function handler(ev) {
   displayInput();
-});
+}
 
 function displayInput() {
   document.getElementById("decimal_result").innerHTML = document.getElementById("binary").value;
