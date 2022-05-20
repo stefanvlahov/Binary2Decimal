577fe21e034ae3b680ffaf333a177e6eca6ae3be
svlahov <stefan.vlahov5@gmail.com>
2022-05-19 21:49:41 -0400

    Adding constraints for binary

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-let binaryInput = document.getElementById("binary");
-let entryForm = document.getElementById("form_submission");
+let binaryInput = document.querySelector('binary');
+let entryForm = document.querySelector('form_submission');
 
-entryForm.addEventListener("submit", false);
+entryForm.noValidate = true;
+// entryForm.addEventListener("submit", handler,false);
 
-binaryInput.addEventListener("", handler, false);
+binaryInput.addEventListener('input', () => {
+  binaryInput.setCustomValidity('');
+  binaryInput.checkValidity();
+});
 
-function handler(ev) {
-  displayInput();
-}
-
-function displayInput() {
-  document.getElementById("decimal_result").innerHTML = document.getElementById("binary").value;
-}
+binaryInput.addEventListener('invalid', () => {
+  if(binaryInput.value === '') {
+    binaryInput.setCustomValidity('Enter a binary number!');
+  } else {
+    binaryInput.setCustomValidity('Binary numbers can only contain 0s and 1s');
+  }
+});
+// function handler(ev) {
+//   displayInput();
+// }
+//
+// function displayInput() {
+//   document.getElementById("decimal_result").innerHTML = document.getElementById("binary").value;
+// }
